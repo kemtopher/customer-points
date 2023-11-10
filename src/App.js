277a5fedd6ba0527f20ae4288aafb6d80bf0f1c0
Christopher Kemeza <ckemeza1@gmail.com>
2023-11-10 18:28:15 -0500

    better error handling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React, { useEffect, useState } from "react";
 import UserDash from "./user-dash/user-dash";
+import "../src/app.css";
 
 const App = () => {
   const [transactions, setTransactions] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   async function getTransactions(url) {
-    const res = await fetch(url);
-    const data = await res.json();
-
-    return data;
+    try {
+      setLoading(true);
+      const res = await fetch(url);
+      if (!res.ok) {
+        setError(true);
+      }
+      const data = await res.json();
+      return data;
+    } catch (err) {
+      setError(true);
+    }
   }
 
   useEffect(() => {
-    getTransactions("http://localhost:4000/transactions").then((res) =>
-      setTransactions(res)
-    );
+    getTransactions("http://localhost:4000/transactions")
+      .then((res) => {
+        setTransactions(res);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(true);
+      });
   }, []);
 
   return (
     <div className="App">
-      <UserDash data={transactions} />
+      <div className="app-stage">
+        <div className="dash-wrapper">
+          {loading ? (
+            <h1>loading data ...</h1>
+          ) : (
+            <UserDash data={transactions} />
+          )}
+        </div>
+      </div>
     </div>
   );
 };
--- src/user-dash/user-dash.js
+++ src/user-dash/user-dash.js
@@ -1,24 +1,24 @@
 import React, { useEffect, useState } from "react";
 
 const UserDash = ({ data }) => {
-  const [purchaseAmount, setPurchaseAmount] = useState(0);
+  const [purchaseTotal, setPurchaseTotal] = useState(0);
   const [points, setPoints] = useState(0);
 
   useEffect(() => {
     data.forEach((transaction) => {
-      const floorAmount = Math.floor(transaction.amount);
+      const roundedAmount = Math.floor(transaction.amount);
 
-      if (floorAmount < 50) return;
+      if (roundedAmount < 50) return;
 
-      if (floorAmount > 100) {
-        const newPoints = (floorAmount - 100) * 2 + 50;
+      if (roundedAmount > 100) {
+        const newPoints = (roundedAmount - 100) * 2 + 50;
         setPoints((prev) => prev + newPoints);
-      } else if (floorAmount > 50 && floorAmount < 100) {
-        const newPoints = floorAmount - 50;
+      } else if (roundedAmount > 50 && roundedAmount < 100) {
+        const newPoints = roundedAmount - 50;
         setPoints((prev) => prev + newPoints);
       }
 
-      setPurchaseAmount((prev) => prev + transaction.amount);
+      setPurchaseTotal((prev) => prev + transaction.amount);
     });
 
     return;
@@ -31,7 +31,7 @@ const UserDash = ({ data }) => {
 
   return (
     <div className="user-dash">
-      <h1>Purchased to date: {CurrencyFormat.format(purchaseAmount)}</h1>
+      <h1>Purchased to date: {CurrencyFormat.format(purchaseTotal)}</h1>
       <h1>Total Points: {points}</h1>
     </div>
   );
