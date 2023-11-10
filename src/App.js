d264d22eb76464379485104959d1c43ee4767d37
Christopher Kemeza <ckemeza1@gmail.com>
2023-11-10 17:00:46 -0500

    Points program logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UserDash from "./user-dash/user-dash";
 
 const App = () => {
+  const [transactions, setTransactions] = useState([]);
+
+  async function getTransactions(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return data;
+  }
+
+  useEffect(() => {
+    getTransactions("http://localhost:4000/transactions").then((res) =>
+      setTransactions(res)
+    );
+  }, []);
+
   return (
     <div className="App">
-      <UserDash />
+      <UserDash data={transactions} />
     </div>
   );
 };
--- src/user-dash/user-dash.js
+++ src/user-dash/user-dash.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const UserDash = () => {
+const UserDash = ({ data }) => {
+  const [purchaseAmount, setPurchaseAmount] = useState(0);
+  const [points, setPoints] = useState(0);
+
+  useEffect(() => {
+    data.forEach((transaction) => {
+      const floorAmount = Math.floor(transaction.amount);
+
+      if (floorAmount < 50) return;
+
+      if (floorAmount > 100) {
+        const newPoints = (floorAmount - 100) * 2 + 50;
+        setPoints((prev) => prev + newPoints);
+      } else if (floorAmount > 50 && floorAmount < 100) {
+        const newPoints = floorAmount - 50;
+        setPoints((prev) => prev + newPoints);
+      }
+
+      setPurchaseAmount((prev) => prev + transaction.amount);
+    });
+
+    return;
+  }, [data]);
+
+  const CurrencyFormat = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
   return (
     <div className="user-dash">
-      <h1>user dashboard</h1>
+      <h1>Purchased to date: {CurrencyFormat.format(purchaseAmount)}</h1>
+      <h1>Total Points: {points}</h1>
     </div>
   );
 };
